145a0803e9c772f38687f9ffe1338c8b89de3c34
Agustin Molina <am.motos@hotmail.com>
2023-09-08 17:52:27 -0300

    resend email

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,22 +1,40 @@
-import Email from '../../emails/welcome';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import Email from "../../emails/welcome";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-   const { firstName,email } = await request.json();
-   await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: email, 
-      subject: 'Nos pondremos en contacto pronto',
+  const { firstName, email } = await request.json();
+  try {
+    const data = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "Hello world",
       react: Email({
         firstName,
       }),
     });
 
-    return NextResponse.json({
-        status: 'success',
-    });
-  
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
+
+// export async function POST(request: Request) {
+//    const { firstName,email } = await request.json();
+//    await resend.emails.send({
+//       from: 'onboarding@resend.dev',
+//       to: email,
+//       subject: 'Nos pondremos en contacto pronto',
+//       react: Email({
+//         firstName,
+//       }),
+//     });
+
+//     return NextResponse.json({
+//         status: 'success',
+//     });
+
+// }
