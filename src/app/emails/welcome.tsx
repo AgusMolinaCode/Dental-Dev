3768ce51b109d78ef1296570a430dd7dcf082f6d
Agustin Molina <am.motos@hotmail.com>
2023-09-08 18:09:05 -0300

    resend email

--- src/app/emails/welcome.tsx
+++ src/app/emails/welcome.tsx
@@ -21,12 +21,6 @@ const Email = ({ firstName }) => {
       <p className="text-lg text-center mb-4">
         Tu mensaje fue recibido exitosamente
       </p>
-      {/* <Button
-        href="https://example.com"
-        className="bg-brand px-3 py-2 font-medium leading-4 text-white"
-      >
-        Click me
-      </Button> */}
     </Tailwind>
   );
 };
